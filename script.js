2bdf092510f9e5795c81fe5fee65a2632dccc9f7
AbduTech <abdulqudoospk737@gmail.com>
2023-09-09 14:11:17 +0500

    updated the code

--- script.js
+++ script.js
@@ -9,23 +9,24 @@ const week = weeks[day];
 const weekElement = document.getElementById("Week");
 weekElement.innerHTML = week;
 
-const localTime = currentTime.toLocaleTimeString();
 
 setInterval(() => {
-    const now = new Date();
-    const utc_Time = now.getUTCHours() * 3600 + now.getUTCMinutes() * 60 + now.getUTCSeconds();
+    const now = Date.now();
+    const utc_Time = now
+
     const utcTimeElement = document.getElementById("utcTime");
     utcTimeElement.innerHTML = utc_Time;
 
-    const localTime = now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
 
-    const localTimeElement = document.getElementById("localTime");
-    localTimeElement.innerHTML = localTime;
+    // const localTime = now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+
+    // const localTimeElement = document.getElementById("localTime");
+    // localTimeElement.innerHTML = localTime;
     // const difference = utc_Time - localTime;
     // const differenceElement = document.getElementById("difference");
     // differenceElement.innerHTML = difference;
 
-}, 1000);
+}, 500);
 
 
 
