267a1aa85736b388858ef7fa115dce95a9314415
AbduTech <abdulqudoospk737@gmail.com>
2023-09-09 09:35:17 +0500

    I have updated my code

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const currentTime = new Date();
 const day = currentTime.getDay();
 const weeks = ["Sunday", "Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
 const week = weeks[day];
-console.log(week);
 
 
 const weekElement = document.getElementById("Week");
@@ -14,13 +13,27 @@ const localTime = currentTime.toLocaleTimeString();
 
 setInterval(() => {
     const now = new Date();
-    const utc_Time = now.toISOString().slice(11, 19);
-    const localTime = now.toLocaleTimeString();
+    const utc_Time = now.getUTCHours() * 3600 + now.getUTCMinutes() * 60 + now.getUTCSeconds();
+    const utcTimeElement = document.getElementById("utcTime");
+    utcTimeElement.innerHTML = utc_Time;
+
+    const localTime = now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+
     const localTimeElement = document.getElementById("localTime");
     localTimeElement.innerHTML = localTime;
-    const utcTime = document.getElementById("utcTime")
-    utcTime.innerHTML = utc_Time;
+    // const difference = utc_Time - localTime;
+    // const differenceElement = document.getElementById("difference");
+    // differenceElement.innerHTML = difference;
+
 }, 1000);
+
+
+
+
+
+
+
+
 
 
 
